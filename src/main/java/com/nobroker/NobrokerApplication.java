b7d07687fe2012a406fe56bae0b8a21abb3216d4
Rajneesh232 <rajneeshpal232@gmail.com>
2024-07-09 11:02:49 +0530

    priview branch

--- src/main/java/com/nobroker/NobrokerApplication.java
+++ src/main/java/com/nobroker/NobrokerApplication.java
@@ -11,7 +11,7 @@ public class NobrokerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NobrokerApplication.class, args);
 	}
-
+//I am a developer
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
